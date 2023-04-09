40fdf7a8a0c75064bcfef8dce564ddd815469b21
mba63 <med.benamer.2015@gmail.com>
2023-04-09 00:27:17 +0000

    view_SecretCode

--- resources/views/Principale/Code_Secret.blade.php
+++ resources/views/Principale/Code_Secret.blade.php
@@ -9,46 +9,61 @@ FSDM
 @endsection
 
 @section('content')
-<div class="container-fluid" id="container-wrapper">
-    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+<!--content -->
 
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            Saisir le Code Secret 
-        </h2>
-        <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-              
-                @if(session('error'))
-                    <div onclick="this.style.display='none'" class="alert-danger">
-                        {{ session('error') }}
+<div class="row justify-content-center">
+    <div class="col-xl-6 col-lg-12 col-md-9">
+        <div class="card shadow-sm my-5">
+            <div class="card-body p-2">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="login-form">
+                            <div class="text-center">
+                                <h1 class="h4 text-gray-900 mb-4">Saisir le Code Secret </h1>
+                            </div>
+                            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                                    <div class="p-6 text-gray-900 dark:text-gray-100">
+                                        @if(session('error'))
+                                            <div onclick="this.style.display='none'" class="alert alert-light alert-dismissible" role="alert">
+                                                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                                    <span aria-hidden="true">×</span>
+                                                </button>
+                                                    {{ session('error') }}
+                                            </div>
+                                        @endif
+                                        <div class="card-body">
+                                            <form method="POST" action="{{route('verifier',['ids' =>  $ids ]) }}">
+                                                @csrf
+                                                <div class="form-group">
+                                                    <input pattern="[0-9]{1,4}" type="text" name="code_saisi" id="code_saisi" class="form-control" placeholder="Entrer le code secret" > 
+                                                </div>
+                                                <div class="form-group">
+                                                    <button type="submit" class="btn btn-primary btn-block" >Valider</button>
+                                                </div>
+                                            </form>
+                                        </div>
+                                            {{--
+                                                <div class="flex items-center justify-end mt-4">
+                                                    @if (Route::has('password.request'))
+                                                        <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="">
+                                                            {{ __('Forgot your password?') }}
+                                                        </a>
+                                                    @endif
+                                                </div>
+                                            --}}
+                                    </div>
+                                </div>
+                            </div>             
+                        </div>
                     </div>
-                @endif
-
-                    <form method="POST" action="{{route('verifier',['ids' =>  $ids ]) }}">
-                        @csrf
-                        <label for="code_saisi">Code secret :</label>
-                        <input pattern="[0-9]{1,4}" type="text" name="code_saisi" id="code_saisi">
-                        <button type="submit">Valider</button>
-                    </form>
-
-       {{--     <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-       </div>
-       --}}
                 </div>
             </div>
         </div>
     </div>
-
-    </div>
 </div>
 
+<!--end content -->
 @endsection
 
 
